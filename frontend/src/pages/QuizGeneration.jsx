210ca8f0ca7f7cf9a6640e4993e3d3587b43bbd4
cperalt <145234956+cperalt@users.noreply.github.com>
2024-08-20 14:26:48 -0400

    Update correct link

--- frontend/src/pages/QuizGeneration.jsx
+++ frontend/src/pages/QuizGeneration.jsx
@@ -21,7 +21,7 @@ const QuizGeneration = ({ setQuiz }) => {
     setLoading(true);
 
     try {
-      const response = await fetch("/generate-quiz", {
+      const response = await fetch("https://lrnr-quiz-backend.vercel.app/generate-quiz", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- frontend/src/pages/QuizPage.jsx
+++ frontend/src/pages/QuizPage.jsx
@@ -41,7 +41,7 @@ const QuizPage = () => {
         const question = quiz.questions[currentQuestionIndex].question;
         const userAnswer = userAnswers[currentQuestionIndex];
 
-        const response = await fetch("/evaluate-answer", {
+        const response = await fetch("https://lrnr-quiz-backend.vercel.app/evaluate-answer", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -84,7 +84,7 @@ const QuizPage = () => {
 
     try {
       if (quiz) {
-        const response = await fetch("/submit-quiz", {
+        const response = await fetch("https://lrnr-quiz-backend.vercel.app/submit-quiz", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
